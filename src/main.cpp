c60182ade7ecfaf4abcbd046b34b7e6729aa0c27
VenousCoot <sfs6304@psu.edu>
2024-11-21 22:45:45 -0500

    Completedish Problem2
    Did a rough draft of Problem2 with the help of AI

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 
 
 #include "q1.h"
+#include "q2.h"
 #include <iostream>
 #include <vector>
+#include <thread>
 
 
 
@@ -35,5 +37,32 @@ int main(){
     cout << "END OF QUESTION 1" << endl;
     cout << "#################################" << endl;
     return 0;
-    
+
+
+
+    cout << "#################################" << endl;
+    cout << "START OF QUESTION 2" << endl;
+    cout << "#################################" << endl;
+// Create an array of five mutexes, each representing a tool
+    std::mutex tools[5];
+    // Create a vector to hold the robot threads
+    std::vector<std::thread> robots;
+
+    // Loop to create and start five robot threads
+    for (int i = 0; i < 5; ++i) {
+        // Create a new robot with its ID and references to two adjacent tools, then start its performTask method in a new thread
+        robots.emplace_back(&Robot::performTask, Robot(i, tools[i], tools[(i + 1) % 5]));
+    }
+
+    // Loop to join all robot threads
+    for (auto& robot : robots) {
+        // Wait for the thread to finish execution
+        robot.join();
+    }
+    cout << "#################################" << endl;
+    cout << "END OF QUESTION 2" << endl;
+    cout << "#################################" << endl;
+    return 0;
 }
+    
+
