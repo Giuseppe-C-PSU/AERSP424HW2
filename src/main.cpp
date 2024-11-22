da1e8e40bc3cd4cef7cfd4068bdce4707d4bd16f
Giuseppe Carbone <gac5517@psu.edu>
2024-11-22 14:50:41 -0500

    First Commit for P4
    Almost Done

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "q1.h"
 #include "q2.h"
 #include "airport_simulation.h"
+#include "q4.h"
 #include <iostream>
 #include <vector>
 #include <thread>
@@ -45,21 +46,21 @@ int main(){
     cout << "#################################" << endl;
     
     // Create an array of five mutexes, each representing a tool
-    std::mutex tools[5];
+    // std::mutex tools[5];
     // Create a vector to hold the robot threads
-    std::vector<std::thread> robots;
+    // std::vector<std::thread> robots;
 
     // Loop to create and start five robot threads
-    for (int i = 0; i < 5; ++i) {
+    // for (int i = 0; i < 5; ++i) {
         // Create a new robot with its ID and references to two adjacent tools, then start its performTask method in a new thread
-        robots.emplace_back(&Robot::performTask, Robot(i, tools[i], tools[(i + 1) % 5]));
-    }
+    //     robots.emplace_back(&Robot::performTask, Robot(i, tools[i], tools[(i + 1) % 5]));
+    // }
 
     // Loop to join all robot threads
-    for (auto& robot : robots) {
+    // for (auto& robot : robots) {
         // Wait for the thread to finish execution
-        robot.join();
-    }
+    //     robot.join();
+    // }
     cout << "#################################" << endl;
     cout << "END OF QUESTION 2" << endl;
     cout << "#################################" << endl;
@@ -103,7 +104,18 @@ int main(){
     cout << "END OF QUESTION 3" << endl;
     cout << "#################################" << endl;    
 
+
+
+    cout << "#################################" << endl;
+    cout << "START OF QUESTION 4" << endl;
+    cout << "#################################" << endl;
+    // Call the function lol
+
+    transfer_orbit();
+
+    cout << "#################################" << endl;
+    cout << "END OF QUESTION 4" << endl;
+    cout << "#################################" << endl;
+
     return 0;
 }
-    
-
---
+++ src/q4.cpp
@@ -0,0 +1,77 @@
+#include <matplot/matplot.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <tuple>
+
+using namespace std;
+using namespace matplot;
+
+// Plotting Transfer orbit because why the fuck not
+
+vector<double> r(const int a, const int e, vector<double>& f, const int mu){
+    vector<double> radius;
+    for(int i = 0; i < f.size(); i++){
+        radius[i] = (a*(1-e^2))/(1+e*cos(f[i]));
+    }
+    return radius;
+};
+
+double p_calc(int r1,int r2,double aMin, double f1, double f2, double delf){
+    if (delf == 0){
+        delf == abs(f2-f1);
+    }
+    double k = r1 * r2 * (1 - cos(delf));
+    double m = r1 * r2 * (1 + cos(delf));
+    double l = r1 + r2;
+    double p = (k * m - 2 * aMin * k * l) / (4 * aMin * m - 2 * aMin * pow(l,2));
+
+    return p;
+};
+
+void transfer_orbit(){
+
+    // Given Parameters:
+    int mu_earth = 398600;
+
+    // Arrival Location Details:
+    int a = 27000;
+    int e = 0.6;
+    int f = deg2rad(30); // ITS FUCKING MATLAB BAYBEEEEEE
+    double r = (a*(1-e^2))/(1+e*cos(f));
+
+    // Departure Location:
+    int d_a = 8000;
+    int d_e = 0.01;
+    int d_f = deg2rad(30); // ITS FUCKING MATLAB BAYBEEEEEE
+    double d_r = (d_a*(1-d_e^2))/(1+d_e*cos(d_f));
+
+    // Transfer Orbit:
+    double delf = abs(d_f - f);
+    double aMin = 0.25 * (d_r + r + sqrt(pow(d_r,2) + pow(r,2) - 2 * r * d_r * cos(delf)));
+    double p = p_calc(d_r,r,aMin,0,0,delf);
+    double time =  p/3600;
+
+    // T orbit time calc
+    double delT = sqrt((pow(aMin,3))/mu_earth) * pi;
+
+    double t_e = sqrt(1-(p/aMin));
+
+    vector <double> d_delf = iota(0, 0.0635, 2*pi);
+    vector <double> a_delf = iota(0, 0.0635, 2*pi);
+    vector <double> t_delf = iota(0, 0.0317, pi);
+
+    vector <double> d_orbit = transform(d_delf, [d_a,d_e](auto x) { return (d_a*(1-pow(d_e,2)))/(1+d_e*cos(x));; });
+    vector <double> a_orbit = transform(d_delf, [a,e](auto x) { return (a*(1-pow(e,2)))/(1+e*cos(x));; });
+    vector <double> t_orbit = transform(d_delf, [aMin,t_e](auto x) { return (aMin*(1-pow(t_e,2)))/(1+t_e*cos(x));; });
+
+    polarplot(d_delf,d_orbit,"c");
+    hold(on);
+    polarplot(a_delf,a_orbit,"m");
+    polarplot(t_delf,t_orbit,"g");
+    hold(off);
+    matplot::legend({"Departure","Arrival","Transfer"});
+
+    show();
+
+}
